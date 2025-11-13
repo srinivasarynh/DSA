d4f2f05294043b9ced93a041d300b5e8f2ba5d12
srinivas <srinivas@srinivass-MacBook-Air.local>
2025-11-13 16:39:47 +0530

    notes added

---
+++ Array_and_String/Frequency_counter/valid_anagram.py
@@ -0,0 +1,47 @@
+# Problem: 242. Valid Anagram
+# LeetCode: https://leetcode.com/problems/valid-anagram/description/
+# Author: srinivas
+# Date: 2025-11-13
+# Language: Python3
+
+from collections import Counter
+
+
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        # Your code here
+        # Brute force
+        """
+        s_dict = dict()
+        t_dict = dict()
+
+        for char in s:
+            s_dict[char] = s_dict.get(char, 0) + 1
+
+        for char in t:
+            t_dict[char] = t_dict.get(char, 0) + 1
+
+        return s_dict == t_dict
+        """
+
+        # Optimized using Counter
+
+        return Counter(s) == Counter(t)
+
+
+if __name__ == "__main__":
+    sol = Solution()
+    tests = [
+        {"input": ("anagram", "nagaram"), "expected": True},
+        {"input": ("rat", "car"), "expected": False},
+        {"input": ("", ""), "expected": True},
+        {"input": ("a", "b"), "expected": False},
+        {"input": ("aacc", "ccac"), "expected": False},
+        {"input": ("åßç", "çßå"), "expected": True},
+    ]
+
+    for i, t in enumerate(tests, 1):
+        result = sol.isAnagram(*t["input"])
+        print(
+            f"Test {i}: result={result}, expected={t['expected']}, {'✅ PASS' if result==t['expected'] else '❌ FAIL'}"
+        )
